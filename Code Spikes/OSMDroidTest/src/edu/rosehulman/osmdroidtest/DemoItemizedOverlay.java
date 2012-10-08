1b03c8d87ebfd2393593a3c78064632feee83937
Sam Kim <kimsj@rose-hulman.edu>
2012-10-08 10:46:29 -0400

    Add OSMDroid Demo project file

---
+++ Code Spikes/OSMDroidTest/src/edu/rosehulman/osmdroidtest/DemoItemizedOverlay.java
@@ -0,0 +1,45 @@
+package edu.rosehulman.osmdroidtest;
+
+import java.util.ArrayList;
+
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.api.IMapView;
+import org.osmdroid.views.overlay.ItemizedOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+
+public class DemoItemizedOverlay extends ItemizedOverlay<OverlayItem> {
+
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	private Context mContext;
+	
+	public DemoItemizedOverlay(Drawable pDefaultMarker,
+			ResourceProxy pResourceProxy, Context context) {
+		super(pDefaultMarker, pResourceProxy);
+		mContext = context;
+	}
+	
+	public void addOverlay(OverlayItem overlay) {
+		mOverlays.add(overlay);
+		populate();
+	}
+
+	public boolean onSnapToItem(int arg0, int arg1, Point arg2, IMapView arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected OverlayItem createItem(int i) {
+		return mOverlays.get(i);
+	}
+
+	@Override
+	public int size() {
+		return mOverlays.size();
+	}
+
+}
