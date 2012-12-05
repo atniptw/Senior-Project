25cb82db85dab70a2b2b27ba4605b374f5b80108
Seth Troisi <troisisa@rose-hulman.edu>
2012-12-05 16:18:45 -0500

    Removed map files updated convert script

--- Code Spikes/maps/convert.py
+++ Code Spikes/maps/convert.py
@@ -4,34 +4,37 @@ import math
 import os
 import subprocess
 
-size_x = 21
-size_y = 14
+squares = 16
 
-max_zoom = int(1 + math.log(max(size_x, size_y), 2))
+max_zoom = int(math.log(squares, 2) + 0.01)
 
-filename = "21x14.jpg"
-folder = "map1"
+filename = "map2.jpg"
+folder = filename[:-4]
+
+os.mkdir(folder)
 os.chdir(folder)
+filename = "../" + filename
 
 print "working on {0}".format(filename)
 print "\tmax_zoom = {0}".format(max_zoom)
 
-for zoom in xrange(max_zoom):
+for zoom in xrange(max_zoom+1):
     print "zoom: {0}".format(zoom)
 
     os.mkdir("{0}".format(zoom))
 
-    ratio = 2 ** ((max_zoom-1) - zoom)
-    for x in xrange(size_x / ratio):
+    ratio = 2 ** (max_zoom - zoom)
+    size = 256 * ratio
+
+    for x in xrange(squares / ratio):
         os.mkdir("{0}/{1}".format(zoom, x))
 
-        for y in xrange(size_y / ratio):
-            size = 256 * ratio
+        for y in xrange(squares / ratio):
             parameter = "{0}x{0}+{1}+{2}".format(size, x * size, y * size)
-            newfile = "{0}/{1}/{2}.jpg".format(zoom, x, y)
+            newfile = "{0}/{1}/{2}.png".format(zoom, x, y)
 
             print "\tcreating tile {0}x{1} with zoom {2}".format(x,y,zoom)
             print "\t\t", parameter
 
-#            command = "convert -crop {0}x{0}+{1}+{2} {3} -resize 256 {4}{5}.jpg"
+#            command = "convert -crop {0}x{0}+{1}+{2} {3} -resize 256 {4}{5}.png"
             subprocess.call(["convert", filename, "-crop", parameter, "-resize", "256", newfile])
